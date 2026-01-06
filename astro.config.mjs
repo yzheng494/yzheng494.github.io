1f8c377b66fe6148f3746f2a824016f92c40922c
Tia Zheng <zheng27quanquan@gmail.com>
2026-01-06 16:44:55 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tiazheng.github.io',
+  site: 'https://yzheng494.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
