30b7bd51fa3310a5f439bf0318a5a41f59bdd42a
圈圈s面包机 <yzheng494@gatech.edu>
2026-01-06 13:59:16 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tiazheng.github.io',
+  base: '/my-website',
   vite: {
     plugins: [tailwindcss()],
   },
